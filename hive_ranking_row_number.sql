68bfd0453c9aab64beb7f624c5761305185daf95
kiran5july <kiran.learn7@gmail.com>
2019-11-24 20:04:31 -0600

    Update hive_ranking_row_number.sql

--- hive_ranking_row_number.sql
+++ hive_ranking_row_number.sql
@@ -1,15 +1,50 @@
 
 
-SeLeCT * from (
-SeLeCT tmp.*, 
- row_number() OVER ( PARTITION BY id ORDER BY score DESC) as row_nbr
- FROM (
-   SeLeCT 'r1' as id, 100 as score union all
-   SeLeCT 'r1' as id, 45 as score  union all
-   SeLeCT 'r1' as id, 55 as score union all
-   SeLeCT 'r2' as id, 66 as score  union all
-   SeLeCT 'r2' as id, 77 as score union all
-   SeLeCT 'r3' as id, 33 as score union all
-   SeLeCT 'r3' as id, 88 as score 
-  ) tmp 
-) tmp2 where row_nbr=1;
+//Rank for the actor by movie-count
+----------DATA(kmdb.km_movie_actor)--------
+actor_name,movie_name
+a,m1
+b,m2
+c,m3
+a,m4
+b,m4
+d,m5
+e,m6
+f,m2
+g,m7
+b,m8
+g,m9
+-----------------------
+
+Output:
++----------+------+---------+-----------+------------+
+|actor_name|mv_cnt|mv_cnt_rn|mv_cnt_rank|mv_cnt_drank|
++----------+------+---------+-----------+------------+
+|b         |3     |1        |1          |1           |
+|g         |2     |2        |2          |2           |
+|a         |2     |3        |2          |2           |
+|f         |1     |4        |4          |3           |
+|e         |1     |5        |4          |3           |
+|d         |1     |6        |4          |3           |
+|c         |1     |7        |4          |3           |
++----------+------+---------+-----------+------------+
+
+
+ WITH mv_counts AS (
+  SELECT actor_name, count(1) AS mv_cnt
+   FROM kmdb.km_movie_actor
+    GROUP BY actor_name
+ )
+ ,act_rnks AS (
+   SELECT actor_name,mv_cnt
+    ,row_number() OVER (ORDER BY mv_cnt DESC) AS mv_cnt_rn
+    ,rank() OVER (ORDER BY mv_cnt DESC) AS mv_cnt_rank
+    ,dense_rank() OVER (ORDER BY mv_cnt DESC) AS mv_cnt_drank
+      FROM mv_counts
+ )
+ SELECT * FROM act_rnks
+  --WHERE mv_cnt_drank<=10
+  ORDER BY mv_cnt_drank
+   LIMIT 10
+;
+  
